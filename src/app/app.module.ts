fbd73851720f84b3e97ce79fc82c8b9af1b7bb1d
Rajiniks <rajiniks1994@gmail.com>
2018-08-30 19:11:30 +0530

    final

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
-import { AppRoutingModule } from "src/app/app-routing.module";
+import { AppRoutingModule } from 'src/app/app-routing.module';
 
 @NgModule({
   declarations: [
@@ -21,33 +21,3 @@ import { AppRoutingModule } from "src/app/app-routing.module";
 })
 
 export class AppModule { }
-
-/*
-import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { AppComponent } from './app.component';
-
-
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-
-@NgModule({
-    bootstrap: [ AppComponent ],
-    declarations: [
-        AppComponent
-    ],
-    imports: [
-        // MUST BE FIRST IMPORT. THIS AUTOMATICALLY IMPORTS BROWSERMODULE, HTTPMODULE, AND JSONPMODULE TOO.
-        RouterModule.forRoot([
-            { path: '', redirectTo: 'home', pathMatch: 'full' },
-           /* { path: 'home', component: HomeComponent },
-            { path: 'login', component: AppComponent },
-            { path: '**', redirectTo: 'home' }
-        ]),
-        FormsModule,
-        ReactiveFormsModule
-    ]
-})
-export class AppModule {
-}
-*/
